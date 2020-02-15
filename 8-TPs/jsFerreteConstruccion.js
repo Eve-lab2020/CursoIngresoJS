7d76d615f957369fbeb74cc86fa33d1be043f177
Eve-lab2020 <60674754+Eve-lab2020@users.noreply.github.com>
2020-02-15 17:12:27 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,79 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+function Rectangulo() {
+
+    var largo;
+    var ancho;
+
+    var calculo;
+    var calculo2;
+
+    largo = parseFloat(document.getElementById("Largo").value);
+    ancho = parseFloat(document.getElementById("Ancho").value);
+
+
+    while (isNaN(largo) || isNaN(ancho) || largo < 0 || ancho < 0) {
+
+        alert("Los datos ingresados son incorrectos, los mismos deben estar expresados en valores numéricos reales.");
+
+        parseFloat(document.getElementById("Largo").value) = largo;
+        parseFloat(document.getElementById("Ancho").value) = ancho;
+    }
+
+    calculo = largo + ancho * 2;
+    calculo2 = (calculo * 3);
+    alert("Para alambrar un terreno de esta longitud, cada hilo que se desee colocar debe tener una largo de " + calculo.toFixed(2) + " mts. Por ejemplo para alambrarlo con tres hilos se necesitaría  un total de : " + calculo2.toFixed(2) + " mts.");
+
 
 }
-function Circulo () 
-{
-	
+
+function Circulo() {
+
+    var radio;
+    var calculo;
+    var calculo2;
+
+    radio = parseFloat(document.getElementById("Radio").value);
+
+    while (isNaN(radio) || radio < 0) {
+        alert("Los datos ingresados son incorrectos, los mismos deben estar expresados en valores numéricos reales.");
+
+        parseFloat(document.getElementById("Radio").value) = radio;
+    }
+
+    calculo = 2 * 3.14 * radio;
+    calculo2 = calculo * 3;
+
+    alert("Para alambrar un terreno circular de radio " + radio + ", se necesitarían : " + calculo.toFixed(2) + " mts de alambre. Por ejemplo para alambrarlo con tres hilos se necesita un total de :" + calculo2.toFixed(2) + " mts.");
+
 }
-function Materiales () 
-{
-	
+
+function Materiales() {
+
+    var largo;
+    var ancho;
+
+    var calculo;
+    var calculo2;
+    var calculo3;
+
+    largo=parseFloat(document.getElementById("Largo").value);
+    ancho=parseFloat(document.getElementById("Ancho").value);
+
+    while(isNaN(largo) || isNaN(ancho) || largo < 0 || ancho < 0){
+
+        alert("Los datos ingresados son incorrectos, los mismos deben estar expresados en valores numéricos reales.");
+
+        parseFloat(document.getElementById("Largo").value) = largo;
+        parseFloat(document.getElementById("Ancho").value) = ancho;
+    }
+
+    calculo=largo*ancho;
+    calculo2=(calculo*2);
+    calculo3=(calculo*3);
+
+    alert("Para hacer un contrapiso de "+calculo.toFixed(2)+" mts², se necesitan : "+calculo2.toFixed(2)+" bolsas de Cemento y " +calculo3.toFixed(2)+" bolsas de Cal.");
+
+
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,73 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+function Sumar() {
+
+    var precio1 = parseFloat(document.getElementById("PrecioUno").value);
+    var precio2 = parseFloat(document.getElementById("PrecioDos").value);
+    var precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+    var suma;
+
+    while (isNaN(precio1) || isNaN(precio2) || isNaN(precio3) || precio1 < 0 || precio2 < 0 || precio3 < 0) {
+
+        alert("Los datos ingresados no son válidos. Por favor revise que los precios ingresados esten expresados en valores numéricos e intentelo nuevamente.");
+
+        parseFloat(document.getElementById("PrecioUno").value) = precio1;
+        parseFloat(document.getElementById("PrecioDos").value) = precio2;
+        parseFloat(document.getElementById("PrecioTres").value) = precio3;
+
+    }
+
+    suma = precio1 + precio2 + precio3;
+
+    alert(" El total de los productos sin IVA es : $" + suma.toFixed(2));
+
+
 }
-function Promedio () 
-{
-	
+function Promedio() {
+
+    var precio1 = parseFloat(document.getElementById("PrecioUno").value);
+    var precio2 = parseFloat(document.getElementById("PrecioDos").value);
+    var precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+    var promedio;
+
+    while (isNaN(precio1) || isNaN(precio2) || isNaN(precio3) || precio1 < 0 || precio2 < 0 || precio3 < 0) {
+
+        alert("Los datos ingresados no son válidos. Por favor revise que los precios ingresados estén expresados en valores numéricos e intentelo nuevamente.");
+
+        parseFloat(document.getElementById("PrecioUno").value) = precio1;
+        parseFloat(document.getElementById("PrecioDos").value) = precio2;
+        parseFloat(document.getElementById("PrecioTres").value) = precio3;
+
+    }
+
+    promedio = (precio1 + precio2 + precio3) / 3;
+
+    alert("El promedio de los precios ingresados es : $" + promedio.toFixed(2));
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+
+    var precio1 = parseFloat(document.getElementById("PrecioUno").value);
+    var precio2 = parseFloat(document.getElementById("PrecioDos").value);
+    var precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+    var calculo;
+    var iva;
+
+    while (isNaN(precio1) || isNaN(precio2) || isNaN(precio3) || precio1 < 0 || precio2 < 0 || precio3 < 0) {
+
+        alert("Los datos ingresados no son válidos. Por favor revise que los precios ingresados estén expresados en valores numéricos e intentelo nuevamente.");
+
+        parseFloat(document.getElementById("PrecioUno").value) = precio1;
+        parseFloat(document.getElementById("PrecioDos").value) = precio2;
+        parseFloat(document.getElementById("PrecioTres").value) = precio3;
+
+    }
+
+    calculo = ((precio1 + precio2 + precio3) * 21) / 100;
+    var iva = (precio1 + precio2 + precio3) + calculo;
+
+    alert("El precio final más IVA es : $" + iva.toFixed(2));
 }
