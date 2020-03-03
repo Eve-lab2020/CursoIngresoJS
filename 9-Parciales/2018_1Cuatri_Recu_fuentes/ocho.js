9915b8fbf8daed6ddf98b7be6a129a6e0d18ecef
Eve-lab2020 <60674754+Eve-lab2020@users.noreply.github.com>
2020-03-03 16:51:11 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,94 @@
-function mostrar()
-{
+function mostrar() {
 
+    var letra;
+    var numero;
+    var respuesta;
+
+    var contadorPares = 0;
+    var contadorImpares = 0;
+
+    var contadorCeros = 0;
+    var contadorPositivos = 0;
+    var acumuladorPositivos = 0;
+    var promedioPositivos;
+    var sumaNegativos = 0;
+
+
+    var numeroMinimo;
+    var letraMinimo;
+    var numeroMaximo;
+    var letraMaximo;
+
+    var flag=0;
+    var flag1 = 0;
+    var flag2 = 0;
+
+
+    do {
+
+        letra = prompt("Ingrese una letra : ");
+        numero = parseInt(prompt("Ingrese un número : "));
+
+        while (isNaN(numero) || numero < -100 || numero > 100) {
+
+            numero = parseInt(prompt("Error. Ingrese un número : "));
+        }
+
+
+        if (numero % 2 == 0) {
+            contadorPares++;
+
+        } else {
+            contadorImpares++;  
+        }
+
+
+
+        if (numero == 0) {
+            contadorCeros++;
+
+        }
+        else if (numero > 0|| flag==0) {
+            contadorPositivos++;
+            acumuladorPositivos = acumuladorPositivos + numero;
+            flag=1;
+
+        } else {
+         
+            sumaNegativos = sumaNegativos + numero;
+        }
+
+
+
+        if (numero > numeroMaximo || flag1 == 0) {
+            numeroMaximo = numero;
+            letraMaximo = letra;
+            flag1 = 1;
+        }
+        
+        if(numero<numeroMinimo||flag2==0){
+            numeroMinimo=numero;
+            letraMinimo=letra;
+            flag2=1;
+        }
+
+
+        respuesta=prompt('Desea continuar?').toLowerCase();
+    } while (respuesta == "s");
+
+
+    promedioPositivos = acumuladorPositivos / contadorPositivos;
+    if(flag==0){promedioPositivos=0}
+
+    document.write("Cantidad de números pares : "+contadorPares+"</br>Cantidad de números impares : "+contadorImpares+"</br>Cantidad de ceros : "+contadorCeros+"</br>Promedio de todos los números positivos ingresados : "+promedioPositivos+"</br>Suma de todos los números negativos : "+sumaNegativos+"</br> Numero Máximo : "+numeroMaximo+" , letra : "+letraMaximo+"</br>Numero Mínimo : "+numeroMinimo+" , letra : "+letraMinimo);
 }
+
+
+/*  Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar)
+hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.*/
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,63 @@
-function mostrar()
-{
+function mostrar() {
+    /*Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) ,
+     el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert:
+   a) El promedio de las notas totales.
+   b) La nota más baja y el sexo de esa persona.
+   c) La cantidad de varones que su nota haya sido mayor o igual a 6.*/
+
+
+    var nota;
+    var sexo;
+    var notaMasBaja=0;
+    var sexoNotaMasBaja;
+    var acumuladorNotas = 0;
+    var PromedioNotas;
+    var contadorVaronesMas6 = 0;
+    var flag = 0;
+
+
+
+    for (i = 0; i < 5; i++) {
+
+        nota = parseFloat(prompt("Ingrese la nota (0 al 10)"));
+
+        while (isNaN(nota) || nota < 0 || nota > 10) {
+
+            nota = parseFloat(prompt("Error. Ingrese la nota (0 al 10)"));
+
+        }
+
+        sexo = prompt("Ingrese el sexo 'f o m ' ");
+
+        while (sexo != "f" && sexo != "m") {
+            sexo = prompt("Error. Ingrese el sexo 'f o m'");
+
+        }
+
+        acumuladorNotas = acumuladorNotas + nota;
+
+        if (nota < notaMasBaja || flag == 0) {
+            notaMasBaja = nota;
+            sexoNotaMasBaja = sexo;
+            flag = 1;
+        }
+
+        if (nota >= 6 && sexo == 'm') {
+            contadorVaronesMas6++;
+        }
+
+        
+
+
+
+    }
+
+
+
+    PromedioNotas = acumuladorNotas / 5;
+
+    alert("Promedio de notas totales : " + PromedioNotas + "\nNota más baja : " + notaMasBaja + ", es de Sexo : " + sexoNotaMasBaja + "\nVarones con nota mayor o igual a 6 : " + contadorVaronesMas6);
+
+
 
 }
