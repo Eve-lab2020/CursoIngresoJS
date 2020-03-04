6da851b867728df0417fd2b70308728c55fbece4
alumno <alumno>
2020-03-03 21:09:11 -0300

    hoy

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
 
+    var marca;
+    var peso;
+    var temperaturaAlmacenamiento;
+    var respuesta;
+
+
+    do{
+        marca=prompt("Ingrese la marca");
+        while(!(marca>='a'&&))
+
+
+
+
+    }while(respuesta==0);
+
 }
